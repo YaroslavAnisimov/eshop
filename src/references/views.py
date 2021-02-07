58cc17a424001ac5bb8b23cad1886b0fac6eac22
Yaroslav Anisimov <yaroslavanisimow@yandex.ru>
2021-02-07 19:14:46 +0100

    7.02.html

--- src/references/views.py
+++ src/references/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+from references.models import Author
+
+def home_page (request):
+    authors = Author.objects.last()
+    context= {"books": books}
+    return render(request, template_name="home.html", context= context)
+
+
