f3e227a30ac09b9c39e1bd0ee20ec567fef92d3e
Yaroslav Anisimov <yaroslavanisimow@yandex.ru>
2021-02-11 20:51:49 +0100

    11.02.2021

---
+++ src/references/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.db.models import fields
+from . import models
+
+class AuthorForm (forms.ModelForm):
+    class Meta:
+        model = models.Author 
+        fields = ('name', 'description' )
+    
+    #name = forms.CharField(
+    #    max_length=50,
+    #    required=True,
+    #   label="Author's name",
+    #    help_text="Input some name"
+    #)
+    
+    #description = forms.CharField(
+    #    label="Author's description",
+    #    required=False,
+    #    widget=forms.Textarea
+    #)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+        if name == "Yaroslav":
+            self.add_error('name', 'This name is forbidden')
+        return cleaned_data
+    
+
