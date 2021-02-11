3fcb317b48cfe961ddb8e92cb08af18548858f0c
Yaroslav Anisimov <yaroslavanisimow@yandex.ru>
2021-02-11 20:52:31 +0100

    11.02

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -79,8 +79,9 @@ WSGI_APPLICATION = 'proj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
+    
 }
 
 
--- src/references/views.py
+++ src/references/views.py
@@ -1,13 +1,85 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy 
+from django.views.generic import DetailView, ListView, DeleteView, CreateView
+
+from . import forms
 
 # Create your views here.
 
 from references.models import Author
 
-def home_page (request):
+def authors_list (request):
     authors = Author.objects.all()
     context= {"authors": authors}
     return render(request, template_name="home.html", context= context)
+
+class AuthorsList (ListView) :
+    model = Author
+
+
+
+def author_detail (request, pk):
+    author = Author.objects.get (pk = pk)
+    context= {"object": author}
+    return render(request, template_name="detail.html", context= context)
+
+class AuthorDetail (DetailView):
+    queryset = Author.objects.all()
+
+
+
+def author_delete (request, pk):
+    author = Author.objects.get (pk = pk)
+    Value = author.Value.all()
+    value_count = Value.count()
+    message = f'Author {author.Author_name} with {value_count} values has just been deleted !'
+    Value.delete()
+    author.delete () 
+    context= {"message": message}
+    return render(request, template_name="delete.html", context= context)
+
+class AuthorDelete(DeleteView):
+    success_url = reverse_lazy ('authors-list-cbv/')
+    model=Author 
+
+
+
+def author_create (request):
+    context = {}
+    if request.method == 'POST':
+        form = forms.AuthorForm(request.POST)
+        if form.is_valid():
+            author = form.save() 
+            return HttpResponseRedirect (reverse ('author-detail', kwargs = {'pk': author.pk }))
+        else:
+            context['form'] = form
+
+    else:
+        context['form'] = forms.AuthorForm()
+
+    return render(request, template_name="create.html", context= context)
+
+class AuthorCreate (CreateView):
+    model = Author
+    success_url = reverse_lazy ('authors-list-cbv/')
+    #form_class = forms.AuthorForm
+    fields = ('name' , 'description')
+ 
+
+def author_update (request):
+    context= {}
+    if request.metod == 'GET':
+        author = Author.objects.get (pk =pk)
+        
+    elif request.method == 'POST':
+        Author_name = request.POST.get ('name')
+        Author_description = request.POST.get ('Author_description')
+        author = Author.objects.get (pk =pk) 
+        author.name = Author_name 
+        author.description = Author_description
+        author.save()
+        return HttpResponseRedirect (reverse ('author-detail', kwargs = {'pk': author.pk }))
+    return render(request, template_name="update.html", context= context)
 
 
