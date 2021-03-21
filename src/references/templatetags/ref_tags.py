79620df8395c360f42245cfc03dd6d060a02e382
YaroslavAnisimov <yaroslavanisimow@yandex.ru>
2021-03-22 00:46:28 +0100

    22-03

--- src/references/templatetags/ref_tags.py
+++ src/references/templatetags/ref_tags.py
@@ -1,22 +1,22 @@
-from django.conf import settings
-from django.utils.safestring import mark_safe
-from django import template
+# from django.conf import settings
+# from django.utils.safestring import mark_safe
+# from django import template
 
-register = template.Library()
+# register = template.Library()
 
-@register.simple_tag
-def company_name(prefix):
-    return prefix + " " + settings.COMPANY_NAME
+# @register.simple_tag
+# def company_name(prefix):
+#     return prefix + " " + settings.COMPANY_NAME
 
 
 
-@register.simple_tag
-def arrow(td_book_name, field_to_sort_on, direction_to_sort_on):
-    if field_to_sort_on == td_book_name and direction_to_sort_on == 'up':
-        return mark_safe('<i class="fas fa-angle-up"></i>')
-    elif field_to_sort_on == td_book_name and direction_to_sort_on == 'down':
-        return mark_safe('<i class="fas fa-angle-up"></i>')
-    return ""
+# @register.simple_tag
+# def arrow(td_book_name, field_to_sort_on, direction_to_sort_on):
+#     if field_to_sort_on == td_book_name and direction_to_sort_on == 'up':
+#         return mark_safe('<i class="fas fa-angle-up"></i>')
+#     elif field_to_sort_on == td_book_name and direction_to_sort_on == 'down':
+#         return mark_safe('<i class="fas fa-angle-up"></i>')
+#     return ""
 
 
 
